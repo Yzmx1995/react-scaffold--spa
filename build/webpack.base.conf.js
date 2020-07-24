2996345e9a5d2c0b9a58cf75a534c27b12a81278
Yzmx1995 <2959961419@qq.com>
2020-07-24 15:17:26 +0800

    fix: 脚手架开发5%

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,1 +1,13 @@
 const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+      app: path.resolve(__dirname, '../src/main.js')
+    },
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: '[name].[hash:8].js',
+      publicPath: '/',
+    },
+}
